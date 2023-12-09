056a6fe2d19e9b595a7b9ed9a6c43f6085fd7dac
Vikaskumarrr <143922604+Vikaskumarrr@users.noreply.github.com>
2023-12-09 23:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length -3, isCapital = false ;
+	let output = "#";
+	while (i<shortHex.length) {
+		
+	}
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
